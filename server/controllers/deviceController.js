dffe2a2c470f16cb2c585e59eb424e43f58eb143
Rinat SHARYPOV <rinat.sharypov@yandex.ru>
2021-08-17 13:33:59 +0700

    server

---
+++ server/controllers/deviceController.js
@@ -0,0 +1,49 @@
+const uuid = require('uuid');
+const path = require('path');
+const {Device} = require('../models/models');
+const ApiError = require('../error/ApiError');
+
+class DeviceController {
+    async create(req, res, next) {
+        try {
+        const {name, price, brandId, typeId, info} = req.body;
+        const {img} = req.files;
+        let fileName = uuid.v4() + ".jpg";
+        img.mv(path.resolve(__dirname, '..', 'static', fileName));
+
+        const device = await Device.create({name, price, brandId, typeId, img: fileName});
+
+        return res.json(device);
+        } catch(e) {
+            next(ApiError.badRequest(e.message));
+        }
+    }
+
+    async getAll(req, res) {
+
+        let {brandId, typeId} = req.query;
+
+        let devices;
+        
+        if (!brandId &&  !typeId) {
+            devices = await Device.findAll();
+        }
+        if (brandId && !typeId) {
+            devices = await Device.findAll({where: {brandId}});
+        }
+        if (!brandId &&  typeId) {
+            devices = await Device.findAll({where: {typeId}});   
+        }
+        if (brandId &&  typeId) {
+            devices = await Device.findAll({where: {typeId, brandId}});
+        }
+
+        return res.json(devices);
+    }
+
+    async getOne(req, res) {
+        
+    }
+}
+
+module.exports = new DeviceController();
